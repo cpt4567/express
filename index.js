15bdb1ef18e85485dbc53ad64b545248db00d54f
kms <cpt4567@gmail.com>
2023-01-11 19:21:03 +0900

    json file read write

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(cors());
 app.use(fileUpload());
 
+
 const port = ( port ) => {
    if(!port){
       return 4000
@@ -20,29 +21,97 @@ const port = ( port ) => {
 
 
 app.get(``,(req,res) =>{{
-   res.send("webcam rest api")
+
+   res.send("webcam rest api") 
+
 }}
 )
 
 app.get(`/webcam`,(req,res) =>{{
 
+    const dir = fs.readdirSync(`${__dirname}/video/webcam` )
+    
+    const filePath = `${__dirname}/video/webcam/${dir[1]}`;
 
-   res.send([{
-      test:"1"
-   }])
+     const videoSize = fs.statSync(filePath).size;
 
+     const CHUNK_SIZE = 10 ** 6; // 1MB
+     const start = 0
+
+     const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+      console.log(start, end);
+     // Create headers
+     const contentLength = end - start + 1;
+     
+     const headers = {
+       "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+       "Accept-Ranges": "bytes",
+       "Content-Length": contentLength,
+       "Content-Type": "video/mp4",
+     };
+   
+     // HTTP Status 206 for Partial Content
+     res.writeHead(206, headers);
+   
+     // create video read stream for this particular chunk
+     const videoStream = fs.createReadStream(filePath, { start, end });
+   
+     // Stream the video chunk to the client
+     videoStream.pipe(res);
+    
 }
 })
 
 app.post(`/webcam`,(req,res) =>{{
 
    const { file } = req.files;
-
-   const day = new Date().toLocaleString()
-     
+   
+   const { end } = JSON.parse(req.body.obj).date;
+  
    const videoType = file.mimetype.replaceAll("video/",".")
 
-   file.mv( `${__dirname}/video/webcam/${day}${videoType}`,
+   const filefetch = `${__dirname}/video/webcam/db.json`;
+
+   const readFileFor = () => {
+      
+      fs.readFile( filefetch , "utf-8" , (err,data) =>{
+
+         if(err){
+            console.log(err)
+         }
+   
+         else{
+            writeDataFor(data)
+         }
+   
+      });
+
+   }
+
+   const writeDataFor = ( data ) => {
+
+      let result = !data ? [] :  JSON.parse(data)
+
+      result.push( JSON.parse(req.body.obj) )
+
+      fs.writeFile(filefetch , "" , "utf-8" ,(err) =>{
+
+         if(err){
+   
+            console.log(err)
+   
+         }
+         else{
+   
+            fs.appendFileSync(filefetch , JSON.stringify(result) , "utf-8" )
+   
+         }
+   
+      })
+
+   }
+
+   file.mv( `${__dirname}/video/webcam/${end}${videoType}`,
       
       (err) => {
        if (err) {
@@ -51,34 +120,128 @@ app.post(`/webcam`,(req,res) =>{{
 
        else{
         
-         fs.readFileSync(`${__dirname}/video/webcam/${day}${videoType}`)
-        
-            /* return res.status(200) */
+         fs.readFileSync(`${__dirname}/video/webcam/${end}${videoType}`)
+            
+         readFileFor()
+
+         return res.status(200)
+
        }
 
      }
-      );
+   );
    
    
 }}
 )
 
+app.get("/webcam_list" , (req,res) => {
+
+   const filefetch = `${__dirname}/video/webcam/db.json`;
+      
+      fs.readFile( filefetch , "utf-8" , (err,data) =>{
+
+         if(err){
+            res.send(err)
+            res.status(500)
+         }
+   
+         else{
+            res.send(!data ? [] : data )
+            res.status(200)
+         }
+   
+      });
+
+   
+})
+
 
 app.get(`/display`,(req,res) =>{{
-   res.send([{
-      test:"1"
-   }])
+   
+   const dir = fs.readdirSync(`${__dirname}/video/display` )
+    
+   const filePath = `${__dirname}/video/display/${dir[1]}`;
+
+    const videoSize = fs.statSync(filePath).size;
+
+    const CHUNK_SIZE = 10 ** 6; // 1MB
+    const start = 0
+
+    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+    // Create headers
+    const contentLength = end - start + 1;
+    
+    const headers = {
+      "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": contentLength,
+      "Content-Type": "video/mp4",
+    };
+  
+    // HTTP Status 206 for Partial Content
+    res.writeHead(206, headers);
+  
+    // create video read stream for this particular chunk
+    const videoStream = fs.createReadStream(filePath, { start, end });
+  
+    // Stream the video chunk to the client
+    videoStream.pipe(res);
+
 }})
 
 app.post(`/display`,(req,res) =>{{
 
    const { file } = req.files;
 
-   const day = new Date().toLocaleString()
+   const { end } = JSON.parse(req.body.obj).date;
      
    const videoType = file.mimetype.replaceAll("video/",".")
 
-   file.mv( `${__dirname}/video/display/${day}${videoType}`,
+   const filefetch = `${__dirname}/video/display/db.json`;
+
+   const readFileFor = () => {
+      
+      fs.readFile( filefetch , "utf-8" , (err,data) =>{
+
+         if(err){
+            console.log(err)
+         }
+   
+         else{
+            writeDataFor(data)
+         }
+   
+      });
+
+   }
+
+   const writeDataFor = ( data ) => {
+
+      let result = !data ? [] :  JSON.parse(data)
+
+      result.push( JSON.parse(req.body.obj) )
+
+      fs.writeFile(filefetch , "" , "utf-8" ,(err) =>{
+
+         if(err){
+   
+            console.log(err)
+   
+         }
+         else{
+   
+            fs.appendFileSync(filefetch , JSON.stringify(result) , "utf-8" )
+            
+         }
+   
+      })
+
+   }
+
+
+
+   file.mv( `${__dirname}/video/display/${end}${videoType}`,
       
       (err) => {
        if (err) {
@@ -87,9 +250,11 @@ app.post(`/display`,(req,res) =>{{
 
        else{
         
-         fs.readFileSync(`${__dirname}/video/display/${day}${videoType}`)
-        
-            /* return res.status(200) */
+         fs.readFileSync(`${__dirname}/video/display/${end}${videoType}`)
+         
+         readFileFor()
+         
+         return res.status(200)
        }
 
      }
@@ -98,6 +263,14 @@ app.post(`/display`,(req,res) =>{{
    
 }}
 )
+
+
+app.get("/display_list" , (req,res) => {
+
+   fs.readFileSync(`${__dirname}/video/display/db.json`)
+
+
+})
 
 
 app.use((req, res, next) => {
